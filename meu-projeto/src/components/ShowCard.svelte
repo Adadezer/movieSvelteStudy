0dd91813c7516103ee64238ce4a1652c2b0cb665
Adadezer <adadezer.iwazaki@veredaeducacao.com>
2026-08-03 16:40:52 -0300

    feat: created fetch API using TVMaze api

---
+++ meu-projeto/src/components/ShowCard.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import type { Show } from '$lib/types/show';
+
+	let { show }: { show: Show } = $props();
+</script>
+
+<div
+	class="mb-5 flex w-fit gap-2 rounded-md border border-[#ff5820] p-2 shadow-md shadow-[#393837]"
+>
+	<p>🎬 {show.title}</p>
+	<p>⭐ {show.rating}</p>
+	<p>ano: {show.year}</p>
+</div>
