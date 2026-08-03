b1ef07c78e2a5361a3bc79e8452d3d75d387c725
Adadezer <adadezer.iwazaki@veredaeducacao.com>
2026-08-03 10:24:41 -0300

    feat: created Search and Movie components

--- meu-projeto/src/routes/+page.svelte
+++ meu-projeto/src/routes/+page.svelte
@@ -1,2 +1,39 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import MovieCard from '../components/MovieCard.svelte';
+	import Search from '../components/Search.svelte';
+
+	type Movie = {
+		id: number;
+		title: string;
+		rating: number;
+		year: number;
+	};
+
+	let movieList: Movie[] = [
+		{
+			id: 1,
+			title: 'Vingadores: Ultimato',
+			rating: 5,
+			year: 2019
+		},
+		{
+			id: 2,
+			title: 'Vingadores: Guerra Infinita',
+			rating: 4,
+			year: 2018
+		},
+		{
+			id: 3,
+			title: 'Vingadores: Era de Ultron',
+			rating: 3,
+			year: 2015
+		}
+	];
+</script>
+
+<div class="p-2">
+	<Search />
+	{#each movieList as movie}
+		<MovieCard {movie} />
+	{/each}
+</div>
